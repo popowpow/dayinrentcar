d2e00d53a9357d528b8ff01d4ec1aedef9f9c56f
Ryan Rafilianto <ryan.rafilianto@sicepat.com>
2023-01-30 22:08:14 +0700

    adjust image in gallery

--- src/pages/gallery.tsx
+++ src/pages/gallery.tsx
@@ -6,31 +6,71 @@ const IMAGE = [
     image: "https://dl.dropboxusercontent.com/s/st5lim9bcnqtjur/3.jpeg?dl=0",
   },
   {
-    image: "https://dl.dropboxusercontent.com/s/7ckmn2cy084n1l2/4.jpeg?dl=0",
-  },
-  {
-    image: "https://dl.dropboxusercontent.com/s/u9cyj7avo80i3k9/5.jpeg?dl=0",
-  },
-  {
-    image: "https://dl.dropboxusercontent.com/s/tsm5j3pfslxdbrj/6.png?dl=0",
-  },
-  {
-    image: "https://dl.dropboxusercontent.com/s/bznif5ajciorxb2/7.jpeg?dl=0",
-  },
-  {
     image: "https://dl.dropboxusercontent.com/s/xbz22pztynpllzu/8.jpeg?dl=0",
   },
   {
-    image: "https://dl.dropboxusercontent.com/s/ov1zjcrafgsrpkh/9.jpeg?dl=0",
+    image:
+      "https://dl.dropboxusercontent.com/s/ps0fodk9v3xsec6/WhatsApp%20Image%202023-01-30%20at%209.24.32%20PM%20%281%29.jpeg?dl=0",
   },
   {
-    image: "https://dl.dropboxusercontent.com/s/rmjh8bxrvcsu9d1/10.jpeg?dl=0",
+    image:
+      "https://dl.dropboxusercontent.com/s/id95oik4nfmsnct/WhatsApp%20Image%202023-01-30%20at%209.24.32%20PM%20%282%29.jpeg?dl=0",
   },
   {
-    image: "https://dl.dropboxusercontent.com/s/zo78897vo3py0rj/11.jpeg?dl=0",
+    image:
+      "https://dl.dropboxusercontent.com/s/3unq5citcfw7lk1/WhatsApp%20Image%202023-01-30%20at%209.24.32%20PM.jpeg?dl=0",
   },
   {
-    image: "https://dl.dropboxusercontent.com/s/ht0h71m0inafrkd/12.jpeg?dl=0",
+    image:
+      "https://dl.dropboxusercontent.com/s/9mwp3g5jxc5qf6p/WhatsApp%20Image%202023-01-30%20at%209.24.31%20PM%20%281%29.jpeg?dl=0",
+  },
+  {
+    image:
+      "https://dl.dropboxusercontent.com/s/99kn9toytsmdern/WhatsApp%20Image%202023-01-30%20at%209.24.28%20PM.jpeg?dl=0",
+  },
+  {
+    image:
+      "https://dl.dropboxusercontent.com/s/nuas7rpac6iuoxb/WhatsApp%20Image%202023-01-30%20at%209.24.29%20PM%20%281%29.jpeg?dl=0",
+  },
+  {
+    image:
+      "https://dl.dropboxusercontent.com/s/kz8g3m91lr986bg/WhatsApp%20Image%202023-01-30%20at%209.24.29%20PM.jpeg?dl=0",
+  },
+  {
+    image:
+      "https://dl.dropboxusercontent.com/s/z983q1kx150ldg2/WhatsApp%20Image%202023-01-30%20at%209.25.16%20PM%20%281%29.jpeg?dl=0",
+  },
+  {
+    image:
+      "https://dl.dropboxusercontent.com/s/s76muk6vw0ipblz/WhatsApp%20Image%202023-01-30%20at%209.25.16%20PM.jpeg?dl=0",
+  },
+  {
+    image:
+      "https://dl.dropboxusercontent.com/s/bpwshtlb7d9s4y6/WhatsApp%20Image%202023-01-30%20at%209.24.31%20PM.jpeg?dl=0",
+  },
+  {
+    image:
+      "https://dl.dropboxusercontent.com/s/ozv63ma07e53sa6/WhatsApp%20Image%202023-01-30%20at%209.24.31%20PM%20%282%29.jpeg?dl=0",
+  },
+  {
+    image:
+      "https://dl.dropboxusercontent.com/s/m2ggndp5rcwtw0p/WhatsApp%20Image%202023-01-30%20at%209.24.33%20PM%20%281%29.jpeg?dl=0",
+  },
+  {
+    image:
+      "https://dl.dropboxusercontent.com/s/1j3gcsh8cbmthnq/11WhatsApp-Image-2023-01-30-at-9.jpg?dl=0",
+  },
+  {
+    image:
+      "https://dl.dropboxusercontent.com/s/bk6cddrxnup6jai/WhatsApp%20Image%202023-01-30%20at%209.24.30%20PM.jpeg?dl=0",
+  },
+  {
+    image:
+      "https://dl.dropboxusercontent.com/s/3mwmzcb5tqyknjl/WhatsApp%20Image%202023-01-30%20at%209.24.29%20PM%20%283%29.jpeg?dl=0",
+  },
+  {
+    image:
+      "https://dl.dropboxusercontent.com/s/u5dsmskzgo5wv05/WhatsApp%20Image%202023-01-30%20at%209.24.29%20PM%20%282%29.jpeg?dl=0",
   },
 ];
 
